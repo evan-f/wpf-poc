cf3a152addbe65ecd4f97e0ac5ccc209fea681a7
Evan <e.faj33@gmail.com>
2022-09-27 09:56:00 -0600

    Cleanup POC files

---
+++ Views/MFXEmbedded.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+
+namespace Views
+{
+    public partial class MFXEmbedded : Component
+    {
+        public MFXEmbedded()
+        {
+            InitializeComponent();
+        }
+
+        public MFXEmbedded(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
